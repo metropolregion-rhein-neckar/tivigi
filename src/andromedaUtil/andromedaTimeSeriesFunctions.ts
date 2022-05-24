bba92d2b8b9a062d4f7c52c08e922508052a30d4
Sebastian Bechtold <mr.bechtold@gmail.com>
2022-05-24 18:11:19 +0200

    Added Andromeda time series functions

---
+++ src/andromedaUtil/andromedaTimeSeriesFunctions.ts
@@ -0,0 +1,218 @@
+// NOTE:
+// These functions implement the basic functionality of loading time series data from the Andromeda broker.
+// You pass in a loading task and get the requested data as a return value. 
+// It is purely procedural/functional code with no side effects and no additional features
+// like client-side caching or "smart" logic to only request data that hasn't been loaded yet.
+// Such features can be implemented elsewhere, on top of the functionality provided here.
+
+// The main thought behind this modular approach is (apart from the well-known fact that it is generally
+// a good idea to split code into small functional units) is that the efficient synchronization of
+// time series data from the broker to the client is a complex topic where we are still far from having
+// found really good solutions. Splitting up the involved functionality into smaller pieces allows us to
+// recombine them in different ways and experiment with different approaches of how to manage this.
+
+
+export interface TimeSeriesLoaderTask {
+    entityId: string,
+    attrs: Array<string>,
+
+    dateStart: Date,
+    dateEnd: Date,
+
+    // These are optional:
+    aggrMethod?: "sum",
+    aggrPeriodDuration?: "day"
+
+}
+
+
+export async function loadTimeSeries(brokerBaseUrl: string, tasks: Array<TimeSeriesLoaderTask>): Promise<any> {
+
+    const promises = []
+
+    for (const task of tasks) {
+        promises.push(loadTimeSeriesInPieces(brokerBaseUrl, task))
+    }
+
+    const results = await Promise.all(promises)
+
+
+    const result: any = {}
+
+    for (const entityId in results) {
+        result[entityId] = results[entityId]
+    }
+
+    return result
+}
+
+
+
+
+async function loadTimeSeriesInPieces(brokerBaseUrl: string, task: TimeSeriesLoaderTask) {
+
+    let result: any = {
+    }
+
+    result[task.entityId] = {}
+
+    // ATTENTION: 
+    // 'dateStart' is the BEGINNING, i.e. the EARLIER date.
+    // 'dateEnd' is the END, i.e. the LATER date.
+
+    const timeAt = task.dateStart.toISOString()
+    const endTimeAt = task.dateEnd.toISOString()
+    const timerel = "between_with_start"
+    const lastN = 0
+
+    let url = `${brokerBaseUrl}/temporal/entities/${task.entityId}/?attrs=${task.attrs.join(",")}&timerel=${timerel}&timeAt=${timeAt}&endTimeAt=${endTimeAt}`
+
+    if (task.aggrMethod != undefined && task.aggrPeriodDuration != undefined) {
+        url += `&options=aggregatedValues&aggrMethods=${task.aggrMethod}&aggrPeriodDuration=${task.aggrPeriodDuration}`
+    }
+    else {
+        url += `&options=temporalValues`
+    }
+
+    if (lastN > 0) {
+        url += "&lastN=" + lastN
+    }
+
+    const res = await fetch(url)
+
+    if (res.status != 200) {
+        //console.log("fail")
+        return
+    }
+
+
+    //#region Parse response JSON
+    const text = await res.text()
+
+    let entityFragment: any = undefined
+
+    try {
+        entityFragment = JSON.parse(text)
+    }
+    catch (e) {
+        console.error("Failed to parse response from: " + res.url)
+        return
+    }
+    //#endregion Parse response JSON
+
+
+
+    // TODO: Min/max should only be updated if the request was successful, but returned an empty
+    // array. This needs to be implemented in the broker first.
+
+    let nextRequestStartDate = new Date(task.dateStart.getTime());
+
+    let attrsToCheck = Array<string>()
+
+    let lastReturnedTimestamp = undefined
+
+    for (const attrName of task.attrs) {
+
+        if (entityFragment[attrName] == undefined) {
+            continue
+        }
+
+        if (result[task.entityId][attrName] == undefined) {
+            result[task.entityId][attrName] = {}
+        }
+
+
+        //#region Aggregated
+        if (task.aggrMethod != undefined && task.aggrPeriodDuration != undefined) {
+
+            if (entityFragment[attrName][task.aggrMethod] == undefined || entityFragment[attrName][task.aggrMethod].length == 0) {
+                continue
+            }
+
+
+            // These are just a shortcut variable for convenience
+            const intervals = entityFragment[attrName][task.aggrMethod]
+
+            // Add data from response to collection of loaded data:
+            for (const interval of intervals) {
+
+                const dateStart = new Date(interval[1])
+                //const dateEnd = new Date(interval[2])
+
+
+                const timestamp = dateStart.getTime()
+                result[task.entityId][attrName][timestamp] = interval[0]
+
+            }
+
+            lastReturnedTimestamp = intervals[intervals.length - 1][0]
+            //#endregion aggregated
+        }
+        else {
+
+            if (entityFragment[attrName].values.length == 0) {
+                continue
+            }
+
+            const values = entityFragment[attrName].values
+
+            for (const kvp of values) {
+                const timestamp = new Date(kvp[0]).getTime()
+                result[task.entityId][attrName][timestamp] = kvp[1]
+            }
+
+            lastReturnedTimestamp = values[values.length - 1][0]
+        }
+
+
+        //#region Check whether another request is required to load the complete times series
+
+        // ATTENTION: This assumes that the returned time series instances are 
+        // ordered by observedAt in DESCENDING order!
+
+        const earliestDateOfCurrentAttribute = new Date(lastReturnedTimestamp)
+
+        if (earliestDateOfCurrentAttribute > task.dateStart) {
+
+            attrsToCheck.push(attrName)
+
+            if (earliestDateOfCurrentAttribute > nextRequestStartDate) {
+                nextRequestStartDate = earliestDateOfCurrentAttribute
+            }
+        }
+
+        //#endregion Check whether another request is required to load the complete times series
+    }
+
+
+
+    if (attrsToCheck.length > 0) {
+
+        const newTask: TimeSeriesLoaderTask = {
+            entityId: task.entityId,
+            attrs: attrsToCheck,
+            aggrMethod: task.aggrMethod,
+            aggrPeriodDuration: task.aggrPeriodDuration,
+            dateStart: task.dateStart,
+            dateEnd: nextRequestStartDate
+        }
+
+        const moreData = await loadTimeSeriesInPieces(brokerBaseUrl, newTask)
+
+        for (const attrName in moreData[task.entityId]) {
+
+            if (result[task.entityId][attrName] == undefined) {
+                result[task.entityId][attrName] = []
+            }
+
+
+            for (const kvp of moreData[task.entityId][attrName].values) {
+                const timestamp = new Date(kvp[0]).getTime()
+                result[task.entityId][attrName][timestamp] = kvp[1]
+            }
+        }
+    }
+
+    return result
+}
+
